e1f3312054d055a1550382fe25bc7a9d644ad722
Prabakaran <k.prabakaran@rifluxyss.com>
2014-02-12 19:24:41 +0530

    sampletest

---
+++ testgit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testgit
+//
+//  Created by Rifluxyss on 2/12/14.
+//  Copyright (c) 2014 Rif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testgitTests/testgitTests.m
@@ -0,0 +1,34 @@
+//
+//  testgitTests.m
+//  testgitTests
+//
+//  Created by Rifluxyss on 2/12/14.
+//  Copyright (c) 2014 Rif. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testgitTests : XCTestCase
+
+@end
+
+@implementation testgitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
